117976b560debd16f00036a515e6e1a2c5554e6b
Artur <1arti00@mail.ru>
2022-06-30 11:48:51 +0300

    почти доделана статистика

--- grsmu_diplom/rating/urls.py
+++ grsmu_diplom/rating/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rating_main_page, name="rating_main_page"),
+    path('departments/', views.rating_departments_page, name="rating_departments_page"),
+    path('departments/<pk>', views.rating_department_detail_page, name="rating_department_detail_page"),
 ]
--- grsmu_diplom/rating/views.py
+++ grsmu_diplom/rating/views.py
@@ -9,18 +9,7 @@ def rating_main_page(request):
     #самый закомменченный и оцененный преподы
     most_comment_teachers = Teacher.objects.annotate(count=Count('CommentFor')).order_by('-count')[0:3]
     most_votes_teachers = Teacher.objects.annotate(count=Count('VoteFor')).order_by('-count')[0:3]
-    #самая комментируемая кафедра
-    comment_departments = Teacher.objects.values('department').annotate(count=Count('CommentFor')).order_by('-count')[0:3]
-    most_comment_departments = {}
-    for department in comment_departments:
-        com_dep = Department.objects.get(id=department["department"])
-        most_comment_departments[com_dep]=department["count"]
-    #самая оцененная кафедра
-    votes_departments = Teacher.objects.values('department').annotate(count=Count('VoteFor')).order_by('-count')[0:3]
-    most_votes_departments = {}
-    for department in votes_departments:
-        votes_dep = Department.objects.get(id=department["department"])
-        most_votes_departments[votes_dep]=department["count"]
+
     #больше всего комментов оставили
     most_comment_user = User.objects.annotate(count=Count('CommentFrom')).order_by('-count')[0:3]
     #больше всего оценок поставили
@@ -43,8 +32,6 @@ def rating_main_page(request):
         "most_comment_user":most_comment_user,
         "most_votes_user":most_votes_user,
         "most_liked_comment":most_liked_comment,
-        "most_comment_departments":most_comment_departments,
-        "most_votes_departments":most_votes_departments,
         "most_liked_users":most_liked_users,
         "users_count":users_count,
         "comments_count":comments_count,
@@ -52,9 +39,48 @@ def rating_main_page(request):
     }
     return render(request, 'rating/rating_main_page.html', context)
 
-def rating_departments(request):
-    average_department_vote = 0
-    # по кафедрам
-    departments = 0
-    # по курсам
-    pass
+def rating_departments_page(request):
+    departments = Department.objects.all()
+
+    # самая комментируемая кафедра
+    comment_departments = Teacher.objects.values('department').annotate(comment_count=Count('CommentFor')).order_by('-comment_count')[0:3]
+    most_comment_departments = {}
+    for department in comment_departments:
+        com_dep = Department.objects.get(id=department["department"])
+        most_comment_departments[com_dep] = department["comment_count"]
+
+    # самая оцененная кафедра
+    votes_departments = Teacher.objects.values('department').annotate(votes_count=Count('VoteFor')).order_by('-votes_count')[0:3]
+    most_votes_departments = {}
+    for department in votes_departments:
+        votes_dep = Department.objects.get(id=department["department"])
+        most_votes_departments[votes_dep] = department["votes_count"]
+
+    context = {
+        "departments": departments,
+        "most_comment_departments": most_comment_departments,
+        "most_votes_departments": most_votes_departments,
+    }
+    return render(request, 'rating/rating_departments_page.html', context)
+
+def rating_department_detail_page(request, pk):
+    department = Department.objects.get(pk=pk)
+    # average_vote of department
+    all_comment_teachers = Teacher.objects.filter(department=department).annotate(count=Count('CommentFor')).exclude(count=0).order_by('-count')
+    # all_comment_teachers = Teacher.objects.filter(department=department).annotate(count=Count('CommentFor')).order_by('-count')
+    top_comment_teachers = all_comment_teachers[0:3]
+    comment_teachers = all_comment_teachers[3:]
+    all_votes_teachers = Teacher.objects.filter(department=department).annotate(count=Count('VoteFor')).exclude(count=0).order_by('-count')
+    top_votes_teachers = all_votes_teachers[0:3]
+    votes_teachers = all_votes_teachers[3:]
+
+    context={
+        "pk":pk,
+        "department":department,
+        "top_comment_teachers":top_comment_teachers,
+        "comment_teachers":comment_teachers,
+        "top_votes_teachers":top_votes_teachers,
+        "votes_teachers":votes_teachers,
+
+    }
+    return render(request, 'rating/rating_department_detail_page.html', context)
